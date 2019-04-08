07f159b705a1ed79fe219c39819e727ce4c685f2
Igor <tak.prosto2013@yandex.ru>
2019-04-08 13:49:18 +0300

    mc_xy

--- test2.cpp
+++ test2.cpp
@@ -31,7 +31,7 @@ int main(int argc, char *argv[])
 
     const int N_MET = 7;
     // число разбиений (для каждого метода)
-    size_t aN[N_MET] = {100, 100, 100, 100, 100, 99};
+    size_t aN[N_MET] = {100, 100, 100, 100, 100, 99, 10000};
     int m = 0;
 
     bool withError = false;
